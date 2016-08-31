42d13282219e263256822e79d18e1e92b69da24d
Florian Brucker <florian.brucker@mb.karlsruhe.de>
2016-08-31 13:35:47 +0200

    Lazy objects.

--- oparl.py
+++ oparl.py
@@ -6,12 +6,14 @@ from __future__ import (absolute_import, division, print_function,
 import json
 
 import dateutil.parser
+import requests
 import six
 
 
 SCHEMA_URLS = [
-    'https://schema.oparl.org/1.0/',  # From standard
+    'https://schema.oparl.org/1.0/',  # From OParl 1.0 standard
     'http://oparl.org/schema/1.0/',   # Used by politik-bei-uns
+    'https://oparl.org/schema/1.0/',  # Used by politik-bei-uns
 ]
 
 
@@ -32,7 +34,20 @@ def _class_from_type_uri(uri):
                          name=name, uri=uri))
 
 
+def _get_json(url):
+    return requests.get(url, verify=False).json()
+
+
 def from_json(data):
+    '''
+    Initialize an OParl object from JSON.
+
+    ``data`` is raw OParl JSON data (either as a Python data
+    structure or as a string).
+
+    Returns an appropriate subclass of ``Object`` initialized using
+    the given data.
+    '''
     if isinstance(data, six.string_types):
         data = json.loads(data)
     if not 'id' in data:
@@ -40,13 +55,32 @@ def from_json(data):
     if not 'type' in data:
         raise ValueError('JSON data does not have a `type` field.')
     cls = _class_from_type_uri(data['type'])
-    obj = cls()
-    for key, value in data.iteritems():
-        if key in cls._DATE_FIELDS:
-            value = dateutil.parser.parse(value).date()
-        if key in cls._DATETIME_FIELDS:
-            value = dateutil.parser.parse(value)
-        obj[key] = value
+    obj = cls(data['id'])
+    obj._init_from_json(data)
+    obj.loaded = True
+    return obj
+
+
+def from_id(id):
+    '''
+    Initialize an OParl object from its ID (URL).
+
+    The object's data is downloaded and parsed. The resulting object is
+    returned.
+    '''
+    return from_json(_get_json(id))
+
+
+def lazy(id, type):
+    '''
+    Create a lazy OParl object.
+
+    The returned object doesn't contain any data (aside from the ID and
+    the type). Call its ``load`` method to download the actual data.
+    '''
+    cls = _class_from_type_uri(type)
+    obj = cls(id)
+    obj['type'] = type
     return obj
 
 
@@ -54,7 +88,6 @@ class Object(dict):
     '''
     Base class for all OParl objects.
     '''
-
     # Fields that have type 'Date'. Their values are automatically
     # parsed from the string representation.
     _DATE_FIELDS = []
@@ -62,6 +95,38 @@ class Object(dict):
     # Fields that have type 'DateTime'. Their values are automatically
     # parsed from the string representation.
     _DATETIME_FIELDS = ['created', 'modified']
+
+    def __init__(self, id):
+        '''
+        Private constructor.
+
+        Use ``from_id`` or ``from_json`` instead.
+        '''
+        self['id'] = id
+        self.loaded = False
+
+    def load(self):
+        if self.loaded:
+            return
+        self._init_from_json(_get_json(self['id']))
+        self.loaded = True
+
+    def _init_from_json(self, data):
+        if not 'id' in data:
+            raise ValueError('JSON data does not have an `id` field.')
+        try:
+            cls = _class_from_type_uri(data['type'])
+        except KeyError:
+            raise ValueError('JSON data does not have a `type` field.')
+        if cls != self.__class__:
+            raise ValueError(('Type from JSON data ({type}) does not match ' +
+                             'instance type.').format(type=data['type']))
+        for key, value in data.iteritems():
+            if key in cls._DATE_FIELDS:
+                value = dateutil.parser.parse(value).date()
+            if key in cls._DATETIME_FIELDS:
+                value = dateutil.parser.parse(value)
+            self[key] = value
 
     def __repr__(self):
         id = self['id']
