37b2bdfa4316f8bf92b7a2b29fbd7f1cf0b35fdc
Florian Brucker <florian.brucker@mb.karlsruhe.de>
2016-09-01 16:46:16 +0200

    Add module docstring.

--- oparl/__init__.py
+++ oparl/__init__.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+'''
+OParl client library.
+
+This package provides tools for easily retrieving information from an
+OParl_ server. OParl is a standard interface for publishing information
+about parliaments and their work.
+
+.. _OParl: https://oparl.org
+
+The main tool of the library is the ``Object`` class and its subclasses.
+They provide wrappers for the various OParl classes (``Body``,
+``Person``, ...) and support lazy loading and automatic conversion of
+OParl data types (e.g. ``date-time``) to Python types
+(``datetime.datetime``).
+
+You will typically start by loading a single object from an OParl server
+using the ``from_id`` function::
+
+    import oparl
+    system = oparl.from_id('https://politik-bei-uns.de/oparl')
+
+If you've already got OParl JSON data you can also use ``from_json``.
+
+Instances of ``Object`` and its subclasses support a read-only dict-
+interface::
+
+    for body in system['body']:
+        print(body['name'])
+
+This library tries to be as compatible with OParl 1.0 as possible but
+does not enforce strict compliance. In some cases non-compliant server
+behavior that has been seen "in the wild" is supported. These cases
+trigger a ``SpecificationWarning``.
+'''
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
